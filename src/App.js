01d16276e09908746106387da7098c635479e2c7
Hattori-Takuma <hattori.ta@icloud.com>
2021-05-09 14:55:05 +0900

    5/9

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
@@ -5,15 +6,51 @@ function App() {
     window.alert('Hello world')
   }
 
-  return (   
-  
-     <div>
-       <button onClick={clickMe}>
-         Hello World
+  const name = 'takuma';
+  const [count, setCount] = useState(0);
+  const increment = () => {
+    setCount(count + 1);
+    console.log(count);
+  };
+
+
+
+
+  return (
+    <div>
+      <button onClick={increment}>いいね！</button>
+      {count}
+
+
+
+
+      <div>
+        <button onClick={clickMe}>
+          Hello World
        </button>
-     </div> 
+        <div>
+          <h1>Hello world</h1>
+          <h2>Hello world</h2>
+          <h3>Hello world</h3>
+          <h4>Hello world</h4>
+          <button
+            onClick={() => window.alert("Hello world")}>button</button>
+
+
+          <input type="text" />
+          <a href="#">a タグ</a>
+          {name}
+        </div>
+      </div>
+    </div>
   );
 }
+
+
+
+
+
+
 
 
 
